6eb86019f5db98000f4d003ed5fd0a3e07a1f7a6
Dayalu Patel <88083299+dayalupatel@users.noreply.github.com>
2021-07-29 15:58:55 +0530

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,23 @@
+<?php
+//get data from form
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$from = 'dayalupatel10@gmail.com';
+$to = "dayalupatel98@gmail.com";
+
+$subject = "New Mail from Brandweb";
+
+$email_body = "Name : $name.\n". 
+                "Email = $email.\n".
+                   "Message = $message.\n";
+
+$headers = "From: $from \r\n";
+$headers .= "Reply-to: $email \r\n";
+
+mail($to,$subject,$email_body,$headers);
+
+header("Location: index.html");
+
+?>
