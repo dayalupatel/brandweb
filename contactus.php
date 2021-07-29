8fb7d461bcad858ec35b7c1d6672e944744f21f3
Dayalu Patel <88083299+dayalupatel@users.noreply.github.com>
2021-07-29 16:23:21 +0530

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -1,22 +1,47 @@
 <?php
-//get data from form
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-$from = 'dayalupatel10@gmail.com';
-$to = "dayalupatel98@gmail.com";
-
-$subject = "New Mail from Brandweb";
-
-$email_body = "Name : $name.\n". 
-                "Email = $email.\n".
-                   "Message = $message.\n";
-
-$headers = "From: $from \r\n";
-$headers .= "Reply-to: $email \r\n";
-
-mail($to,$subject,$email_body,$headers); or die("Error!");
-echo "Thank You!";
-
+$statusMsg = '';
+$msgClass = '';
+if(isset($_POST['submit'])){
+    // Get the submitted form data
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    // Check whether submitted data is not empty
+    if(!empty($email) && !empty($name) && !empty($message)){
+        
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+            $msgClass = 'errordiv';
+        }else{
+            // Recipient email
+            $toEmail = 'dayalupatel98@gmail.com';
+            $emailSubject = 'Contact Request Submitted by '.$name;
+            $htmlContent = '<h2>Contact Request Submitted</h2>
+                <h4>Name</h4><p>'.$name.'</p>
+                <h4>Email</h4><p>'.$email.'</p>
+                <h4>Message</h4><p>'.$message.'</p>';
+            
+            // Set content-type header for sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            
+            // Additional headers
+            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
+            
+            // Send email
+            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
+                $statusMsg = 'Your contact request has been submitted successfully !';
+                $msgClass = 'succdiv';
+            }else{
+                $statusMsg = 'Your contact request submission failed, please try again.';
+                $msgClass = 'errordiv';
+            }
+        }
+    }else{
+        $statusMsg = 'Please fill all the fields.';
+        $msgClass = 'errordiv';
+    }
+}
 ?>
